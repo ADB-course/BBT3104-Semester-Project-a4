6f282887b7302bf2d8d8e3950e3082a2564a8113
keith-oruo <keith.oruo@strathmore.edu>
2024-11-19 22:54:06 +0300

    fixing numerical sql error

--- 1_database/script.sql
+++ 1_database/script.sql
@@ -1,15 +1,15 @@
 -- Write your SQL code here
 -- Create the database
-CREATE DATABASE `153378_project`;
+CREATE DATABASE `project_153378`;
 
 -- Use the created database
-USE `153378_project`;
+USE `project_153378`;
 
 -- Grant all privileges to group members
-GRANT ALL PRIVILEGES ON `153378_project`.* TO `166222`@`%`;
+GRANT ALL PRIVILEGES ON `project_153378`.* TO `166222`@`%`;
 
-GRANT ALL PRIVILEGES ON `153378_project`.* TO `169003`@`%`;
+GRANT ALL PRIVILEGES ON `project_153378`.* TO `169003`@`%`;
 
-GRANT ALL PRIVILEGES ON `153378_project`.* TO `169975`@`%`;
+GRANT ALL PRIVILEGES ON `project_153378`.* TO `169975`@`%`;
 
-GRANT ALL PRIVILEGES ON `153378_project`.* TO `169547`@`%`;
+GRANT ALL PRIVILEGES ON `project_153378`.* TO `169547`@`%`;
